8f4244b8afbd7425422452103ec7c468f195adbe
kauzarc <jujurolland91@gmail.com>
2019-12-22 18:41:54 +0100

    bug correction

--- commandline.py
+++ commandline.py
@@ -1,20 +1,24 @@
 import random
 
+
 def help_command(text):
     result = "HELP :\n"
+    result += "\n"
+    result += "help : --> show help\n"
     result += "\n"
     result += "test : --> a testing command\n"
     result += "\n"
     result += "random : @arg an integer --> pick a random number between 0 and @arg\n"
     return result
 
+
 def no_command(text):
     return "Message invalide mother fucker !"
+
 
 def test_command(text):
     return "This command work !"
 
+
 def random_command(text):
-    if int(text[7:]):
-        return str(random.randint(0, int(text[7:])))
-    return ""
+    return "nique sa mere"
