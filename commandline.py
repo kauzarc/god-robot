5275bec730763e177e53cba4391019a6c1d2d2c0
kauzarc <jujurolland91@gmail.com>
2019-12-22 13:38:06 +0100

    help syntaxe update

--- commandline.py
+++ commandline.py
@@ -3,8 +3,9 @@ import random
 def help_command(text):
     result = "HELP :\n"
     result += "\n"
-    result += "test : a testing command\n"
-    result += "random : @arg an integer     pick a random number between 0 and @arg\n"
+    result += "test : --> a testing command\n"
+    result += "\n"
+    result += "random : @arg an integer --> pick a random number between 0 and @arg\n"
     return result
 
 def no_command(text):
