c66a6b7e7c10bc8820dd182b5efb88410e977a8e
kauzarc <jujurolland91@gmail.com>
2019-12-22 23:28:43 +0100

    log max size

--- commandline.py
+++ commandline.py
@@ -24,6 +24,8 @@ def log_command(text, file_name):
         for line in f.readlines():
             result += line + "\n -------------------- \n"
         f.close()
+        if len(result) > 500:
+            result = result[:500]
 
     elif text[4:] == "clear":
         f = open(file_name, "w")
