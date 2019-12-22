8d003fee8600c39ba61d170656f493e40d598b69
kauzarc <jujurolland91@gmail.com>
2019-12-22 13:34:44 +0100

    test help

--- commandline.py
+++ commandline.py
@@ -4,7 +4,7 @@ def help_command(text):
     result = "HELP :\n"
     result += "\n"
     result += "test : a testing command\n"
-    result += "random : @arg an integer\n"
+    result += "random : @arg an integer     pick a random number between 0 and @arg\n"
     return result
 
 def no_command(text):
